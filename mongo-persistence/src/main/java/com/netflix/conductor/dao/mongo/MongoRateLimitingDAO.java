b959254448e2d79e706d31224b9c012b6afb3c99
Vasu Sheoran <vasusheoran92@gmail.com>
2021-02-24 19:43:23 +0530

    Added MongoRateLimitingDAO

---
+++ mongo-persistence/src/main/java/com/netflix/conductor/dao/mongo/MongoRateLimitingDAO.java
@@ -0,0 +1,27 @@
+package com.netflix.conductor.dao.mongo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.conductor.common.metadata.tasks.Task;
+import com.netflix.conductor.common.metadata.tasks.TaskDef;
+import com.netflix.conductor.core.config.Configuration;
+import com.netflix.conductor.dao.RateLimitingDAO;
+
+public class MongoRateLimitingDAO extends BaseMongoDAO implements RateLimitingDAO {
+
+	private static final Logger logger = LoggerFactory.getLogger(MongoRateLimitingDAO.class);
+	
+	public MongoRateLimitingDAO(ObjectMapper objectMapper, Configuration config) {
+		super(objectMapper, config);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean exceedsRateLimitPerFrequency(Task task, TaskDef taskDef) {
+		logger.info(MongoRateLimitingDAO.class.getName() + " is ready to serve");
+		return false;
+	}
+
+}
